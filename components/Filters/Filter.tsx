1a7901f3deb397a7df83df9ae4b1b9f59e56a478
kokohany655 <kokoh2370@gmail.com>
2022-12-07 19:25:23 +0200

    any

--- components/Filters/Filter.tsx
+++ components/Filters/Filter.tsx
@@ -1,4 +1,4 @@
-import { HStack, Icon, Text, useDisclosure } from "@chakra-ui/react";
+import { Accordion, AccordionButton, AccordionItem, AccordionPanel, Box, Checkbox, Flex, HStack, Icon, Input, InputGroup, InputLeftElement, Text, useDisclosure, VStack } from "@chakra-ui/react";
 
 import { IoFilter } from "react-icons/io5";
 
@@ -11,6 +11,7 @@ import {
   ModalBody,
   ModalCloseButton,
 } from "@chakra-ui/react";
+import { AddIcon, MinusIcon, PhoneIcon } from "@chakra-ui/icons";
 
 const Filter = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -21,13 +22,82 @@ const Filter = () => {
       <Text as="button" onClick={onOpen}>
         Filter
       </Text>
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={onClose} size='xs'>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Filter</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            
+          <Accordion allowMultiple >
+            <AccordionItem>
+                {({ isExpanded }) => (
+                    <>
+                    <Text>
+                        <AccordionButton p='10px 0'>
+                            <Box letterSpacing={'.2px'} fontWeight={'600'} fontSize={'15px'} flex='1' textAlign='left'>
+                               Availability
+                            </Box>
+                        {isExpanded ? (
+                        <MinusIcon fontSize='10px' />
+                        ) : (
+                        <AddIcon fontSize='10px' />
+                            )}
+                        </AccordionButton>
+                    </Text>
+                <AccordionPanel pb={4}>
+                  <Flex flexDir={'column'} fontWeight='600' >
+                  <Checkbox colorScheme={'red'}  mb='5px' opacity={'.9'}>in stock</Checkbox>
+                  <Checkbox colorScheme={'red'} opacity={'.9'}>out fot stock</Checkbox>
+                  </Flex>
+                </AccordionPanel>
+            </>
+            )}
+            </AccordionItem>
+            </Accordion>
+            <Accordion allowMultiple >
+            <AccordionItem>
+                {({ isExpanded }) => (
+                    <>
+                    <Text>
+                        <AccordionButton p='10px 0'>
+                            <Box letterSpacing={'.2px'} fontWeight={'600'} fontSize={'15px'} flex='1' textAlign='left'>
+                               Price
+                            </Box>
+                        {isExpanded ? (
+                        <MinusIcon fontSize='10px' />
+                        ) : (
+                        <AddIcon fontSize='10px' />
+                            )}
+                        </AccordionButton>
+                    </Text>
+                <AccordionPanel pb={4}>
+                  <Flex flexDir={'column'} fontWeight='600' >
+                    <Text fontSize={'13px'} mb='10px' fontWeight={'600'} opacity='.7'>From</Text>
+                  <InputGroup mb={'7px'}>
+                  <InputLeftElement
+                  pointerEvents='none'
+                  color='gray.300'
+                  fontSize='1.2em'
+                  children='$'
+                  />
+                  <Input placeholder='Enter amount' />
+                  </InputGroup>
+                  <Text fontSize={'13px'} mb='10px' fontWeight={'600'} opacity='.7'>To</Text>
+                  <InputGroup>
+                  <InputLeftElement
+                  pointerEvents='none'
+                  color='gray.300'
+                  fontSize='1.2em'
+                  children='$'
+                  />
+                  <Input placeholder='Enter amount' />
+                  </InputGroup>
+                  </Flex>
+                </AccordionPanel>
+            </>
+            )}
+            </AccordionItem>
+            </Accordion>
           </ModalBody>
           <ModalFooter></ModalFooter>
         </ModalContent>
--- components/Filters/SortBy.tsx
+++ components/Filters/SortBy.tsx
@@ -1,12 +1,31 @@
-import { HStack, Icon, Text } from "@chakra-ui/react";
+import { Button, HStack, Icon, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure, VStack } from "@chakra-ui/react";
 
 import { BiSortAlt2 } from "react-icons/bi";
 
 const SortBy = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <HStack>
       <Icon as={BiSortAlt2} />
-      <Text as="button">Soft By</Text>
+      <Text as="button" onClick={onOpen}>Soft By</Text>
+      <Modal size={'xs'}  isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Sort By</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody >
+             <VStack spacing={'15px'}>
+             <Button w='100%' colorScheme={'red'}>Featured</Button>
+             <Button w='100%' colorScheme={'red'}>Best Selling</Button>
+             <Button w='100%' colorScheme={'red'}>Price, Low To Heigh</Button>
+             <Button w='100%' colorScheme={'red'}>Price, Heigh To Low</Button>
+             <Button w='100%' colorScheme={'red'}>Date , Old To New</Button>
+             <Button w='100%' colorScheme={'red'}>Date, New To Old</Button>
+             </VStack>
+          </ModalBody>
+          <ModalFooter></ModalFooter>
+        </ModalContent>
+      </Modal>
     </HStack>
   );
 };
